6405b7fb1997e0b0bf628e4d85cb52a0444c806c
minseoky <minseok184@gmail.com>
2024-08-10 03:03:32 +0900

    [feat]: Resolves #7 유저 추가 정보 기입 여부 확인 api 추가

---
+++ src/main/java/com/easymap/easymap/handler/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.easymap.easymap.handler.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/easymap/easymap/service/UserServiceImpl.java
+++ src/main/java/com/easymap/easymap/service/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package com.easymap.easymap.service;
 
+import com.easymap.easymap.config.CustomUserDetails;
 import com.easymap.easymap.dto.request.user.UserNicknameDuplicateRequestDTO;
+import com.easymap.easymap.entity.User;
+import com.easymap.easymap.handler.exception.ResourceNotFoundException;
 import com.easymap.easymap.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,4 +28,35 @@ public class UserServiceImpl implements UserService{
         return userRepository.existsByNicknameNative(userNicknameDuplicateRequestDTO.getNickname());
 
     }
+
+    @Override
+    public List<String> userAdditionalInfoCheck() {
+        String userEmail = findUserEmailFromJwt();
+        User user = userRepository.findByEmail(userEmail).orElseThrow(
+                () -> new ResourceNotFoundException("User not found with email: " + userEmail)
+        );
+        List<String> requireList = new ArrayList<>();
+        if(user.getBirthdate() == null){
+            requireList.add("birthdate");
+        }
+        if(user.getGender() == null){
+            requireList.add("gender");
+        }
+
+        return requireList;
+    }
+
+    /**
+     * SecurityContextHolder에 저장된 유저 정보 추출
+     * @return
+     */
+    private String findUserEmailFromJwt() {
+        String userEmail = "default@default.com";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            userEmail = authentication.getName();
+        }
+        return userEmail;
+    }
+
 }
