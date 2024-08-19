0619c66904a1d9d614e92aa768f5c4bb95b2d2c3
makadamia0055 <makend91@naver.com>
2024-08-19 18:09:24 +0900

    [feat]: 유저 루트 받아서 s3에 올리는 로직 구현

--- src/main/java/com/easymap/easymap/service/MapService.java
+++ src/main/java/com/easymap/easymap/service/MapService.java
@@ -1,9 +1,13 @@
 package com.easymap.easymap.service;
 
+import com.easymap.easymap.dto.request.map.UserRoutePostRequestDTO;
 import com.easymap.easymap.dto.response.map.MapPoisDTO;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
 public interface MapService {
     List<MapPoisDTO> getPoisOnMap(List<Double> bbox);
+
+    Long postUserRoute(UserRoutePostRequestDTO userRoutePostRequestDTO, UserDetails userDetails);
 }
--- src/main/java/com/easymap/easymap/service/MapServiceImpl.java
+++ src/main/java/com/easymap/easymap/service/MapServiceImpl.java
@@ -1,21 +1,31 @@
 package com.easymap.easymap.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.easymap.easymap.dto.request.map.UserRoutePostRequestDTO;
 import com.easymap.easymap.dto.response.map.MapPoisDTO;
 import com.easymap.easymap.entity.Poi;
+import com.easymap.easymap.entity.User;
+import com.easymap.easymap.entity.UserRoute;
 import com.easymap.easymap.entity.category.Category;
-import com.easymap.easymap.entity.category.DetailedCategory;
-import com.easymap.easymap.repository.CategoryRepository;
+import com.easymap.easymap.handler.exception.ResourceNotFoundException;
 import com.easymap.easymap.repository.DetailedCategoryRepository;
 import com.easymap.easymap.repository.PoiRepository;
+import com.easymap.easymap.repository.UserRepository;
+import com.easymap.easymap.repository.UserRouteRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,7 +34,16 @@ public class MapServiceImpl implements MapService{
 
     private final PoiRepository poiRepository;
 
+    private final UserRepository userRepository;
+
     private final DetailedCategoryRepository detailedCategoryRepository;
+
+    private final UserRouteRepository userRouteRepository;
+
+    private final AmazonS3 amazonS3Client;
+
+    @Value("${aws.s3.user-rawdata-bucket-name}")
+    private String bucketName;
 
 
 
@@ -53,5 +72,49 @@ public class MapServiceImpl implements MapService{
 
 
         return collect;
+    }
+
+    @Override
+    public Long postUserRoute(UserRoutePostRequestDTO userRoutePostRequestDTO, UserDetails userDetails) {
+
+        User user = userRepository.findUserByEmailAndDeactivationDateIsNull(userDetails.getUsername()).orElseThrow(() -> new ResourceNotFoundException("no user such as : " + userDetails.getUsername()));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(user.getUserId()).append(",");
+        sb.append(userRoutePostRequestDTO.getStartTime().toString()).append(",");
+        sb.append(userRoutePostRequestDTO.getHop()).append(",");
+
+        String collect = userRoutePostRequestDTO.getData().stream().map(userRouteCoordiDTO -> "(" + userRouteCoordiDTO.getLat() + "," + userRouteCoordiDTO.getLng() + ")")
+                .collect(Collectors.joining(","));
+        sb.append(collect);
+
+        String csvData = sb.toString();
+
+
+        String fileName = user.getUserId()+"-"+userRoutePostRequestDTO.getStartTime().toString()+ UUID.randomUUID().toString()+".csv";
+
+        try(InputStream inputStream = new ByteArrayInputStream(csvData.getBytes(StandardCharsets.UTF_8))){
+            ObjectMetadata metadata = new ObjectMetadata();
+
+            metadata.setContentType("text/csv");
+            metadata.setContentLength(csvData.length());
+
+            amazonS3Client.putObject(bucketName, fileName, inputStream, metadata);
+
+            UserRoute userRoute = UserRoute.builder()
+                    .user(user)
+                    .fileName(fileName)
+                    .createdAt(userRoutePostRequestDTO.getStartTime())
+                    .build();
+
+            UserRoute save = userRouteRepository.save(userRoute);
+            return save.getId();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return -1L;
+        }
+
+
     }
 }
